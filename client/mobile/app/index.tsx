3b2682e5d0729ea5c7ebc2a1eed0e33b4dd98d73
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-19 21:38:34 +0600

    first page

--- client/mobile/app/index.tsx
+++ client/mobile/app/index.tsx
@@ -1,15 +1,41 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Index() {
+const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={style.container}>
+      <LinearGradient
+        colors={['#00C6FF', '#0072FF', "#ffffff"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={style.box}
+      />
     </View>
   );
-}
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#071B49',
+  },
+  box: {
+    width: 334,
+    height: 334,
+    borderRadius: 40,
+    overflow: 'hidden',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+
+export default Index;

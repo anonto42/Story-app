8d4c02387473cd4aaaa2a3480a75585c1d798f41
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-21 20:49:10 +0600

    main layout page with custom splash screen adn inti hew fonts

--- client/mobile/app/_layout.tsx
+++ client/mobile/app/_layout.tsx
@@ -1,5 +1,57 @@
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import * as SplashScreen from 'expo-splash-screen';
+import { View, Image, StyleSheet } from "react-native";
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [appReady, setAppReady] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      // await new Promise(resolve => setTimeout(resolve, 2000));
+      
+      await Font.loadAsync({
+        "Lora-Bold": require('../assets/fonts/Lora-Bold.ttf'),
+        "Lora-Regular": require('../assets/fonts/Lora-Regular.ttf'),
+        "Lora-Medium": require('../assets/fonts/Lora-Medium.ttf'),
+        "Lora-Italic": require('../assets/fonts/Lora-Italic.ttf'),
+      })
+      
+      await SplashScreen.hideAsync();
+      setAppReady(true);
+    };
+
+    prepare();
+  }, []);
+
+  if (!appReady) {
+    return (
+      <View style={styles.splash}>
+        <Image
+          source={require('../assets/images/main.png')}
+          style={styles.logo}
+        />
+      </View>
+    );
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
+
+const styles = StyleSheet.create({
+  splash: {
+    flex: 1,
+    backgroundColor: '#071B49',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 334,
+    height: 334,
+    resizeMode: 'contain',
+    marginBottom: 40,
+  },
+});

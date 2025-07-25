3c34266ddb96c17f59cca455f4dc4d347a850d62
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-25 19:16:06 +0600

    this page will created later

---
+++ client/mobile/app/(pages)/createnewpassword.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const createnewpassword = () => {
+  return (
+    <View>
+      <Text>createnewpassword</Text>
+    </View>
+  )
+}
+
+export default createnewpassword

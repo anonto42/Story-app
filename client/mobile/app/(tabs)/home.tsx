7ffae8fa17d48433db4323e0e82b708cf9e544af
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-25 23:17:58 +0600

    Workin on this page

--- client/mobile/app/(tabs)/home.tsx
+++ client/mobile/app/(tabs)/home.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, StyleSheet, ScrollView } from 'react-native';
+import React from 'react';
+import MainWrapper from '@/components/layout/MainWrapper';
+import HomeTopBar from '@/components/ui/HomeTopBar';
+import CartType from '@/components/screen/CartType';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <MainWrapper>
+      <HomeTopBar />
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        
+        <CartType name="Children Story" />
 
-export default Home
+      </ScrollView>
+    </MainWrapper>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  scrollContent: {
+    paddingTop: 70, // Give space for top bar
+    paddingHorizontal: 16,
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+  },
+});

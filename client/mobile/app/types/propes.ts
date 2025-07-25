e857c09caa3d25b6ff721a42e5196dc38559c093
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-25 23:18:13 +0600

    language types added

--- client/mobile/app/types/propes.ts
+++ client/mobile/app/types/propes.ts
@@ -1,9 +1,10 @@
+import { FlagProps } from "./flag";
 
 export type CartProps = {
     title: string;
     timeFrame: string;
     view: string;
     creator: string;
-    language: string;
+    language: FlagProps;
     thumbel: string;
 }

59018b1f16d4c168b5bd7a627b177520b15bdadb
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-25 22:10:11 +0600

    new app wraper added

--- client/mobile/app/(tabs)/profile.tsx
+++ client/mobile/app/(tabs)/profile.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import MainWrapper from '@/components/layout/MainWrapper'
 
-const profile = () => {
+const Profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <MainWrapper>
+      <View style={styles.container}>
+        <Text>profile</Text>
+      </View>
+    </MainWrapper>
   )
 }
 
-export default profile
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+export default Profile
